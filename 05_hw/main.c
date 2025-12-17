15f4ec10de2d9149636d94e3f0fff681a4da758f
Erterion <79190617+Erterion@users.noreply.github.com>
2025-12-17 20:25:33 +0300

    Update main.c
    Обновление main с учетом обновленного кода.

--- 05_hw/main.c
+++ 05_hw/main.c
@@ -1,19 +1,21 @@
 #include "stack.h"
-
 #include <stdio.h>
 
 int main() {
     Stack stack;
     initStack(&stack);
 
-    push(&stack, 10);
     push(&stack, 20);
+    push(&stack, 10);
     push(&stack, 30);
 
     printf("After pushing elements:\n");
     traverseStack(&stack);
 
-    pop(&stack);
+    int popped_value;
+    if (pop(&stack, &popped_value)) {
+        printf("Popped value: %d\n", popped_value);
+    }
 
     printf("After popping an element:\n");
     traverseStack(&stack);
@@ -21,7 +23,8 @@ int main() {
     Node* searchResult = searchByValue(&stack, 20);
     if (searchResult != NULL) {
         printf("Element with value 20 found.\n");
-    } else {
+    }
+    else {
         printf("Element with value 20 not found.\n");
     }
 
@@ -30,7 +33,13 @@ int main() {
         printf("Top element: %d\n", topElement->data);
     }
 
+    printf("Peek top element: %d\n", top(&stack));
+
+    while (!isEmpty(&stack)) {
+        pop(&stack, &popped_value);
+        printf("Cleaning: popped %d\n", popped_value);
+    }
+
+    destroyStack(&stack);
     return 0;
 }
-
-
